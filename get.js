4c4e1ea646174102c78f5323c776574f92b174fa
Gregor Aisch <gka@users.noreply.github.com>
2019-04-03 18:45:53 +0200

    add comments

--- get.js
+++ get.js
@@ -11,12 +11,13 @@
  */
 export default function(object, key = null, _default = null) {
     if (!key) return object;
-    // get metadata
+    // expand keys
     const keys = key.split('.');
     let pt = object;
 
     for (let i = 0; i < keys.length; i++) {
         if (pt === null || pt === null) break; // break out of the loop
+        // move one more level in
         pt = pt[keys[i]];
     }
     return pt === undefined || pt === null ? _default : pt;
