163c57bab73aecd4352daea1c91734cfae55f86d
Gregor Aisch <gka@users.noreply.github.com>
2019-04-25 17:56:48 +0200

    add one more test

--- colorLightness.test.js
+++ colorLightness.test.js
@@ -5,6 +5,10 @@ test('white lightness is 100', t => {
     t.is(Math.round(colorLightness('#ffffff')), 100);
 });
 
+test('hotpink lightness is 58', t => {
+    t.is(Math.round(colorLightness('#ff3399')), 58);
+});
+
 test('black lightness is 0', t => {
     t.is(Math.round(colorLightness('#000000')), 0);
 });
