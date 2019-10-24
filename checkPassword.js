aad77d9282898d7d158792faa6d1d0cd8cacd9f3
Gregor Aisch <gka@users.noreply.github.com>
2019-10-24 14:51:30 +0200

    require passwords to be at least 10 characters

--- checkPassword.js
+++ checkPassword.js
@@ -1,15 +1,17 @@
 /* global dw */
+const MIN_PASSWORD_LENGTH = 10;
+
 export default function(curPwd, pwd, pwd2) {
     var msg;
     if (curPwd === '') {
         msg = dw.backend.messages.provideCurPwd;
-    } else if (pwd.length < 4) {
-        msg = dw.backend.messages.pwdTooShort;
+    } else if (pwd.length < MIN_PASSWORD_LENGTH) {
+        msg = dw.backend.messages.pwdTooShort.replace('%num', MIN_PASSWORD_LENGTH);
     } else if (pwd !== pwd2) {
         msg = dw.backend.messages.pwdMismatch;
     }
     if (msg) {
         return msg;
     }
-    return true;
+    return false;
 }
