6db85093aa0dfb46636feb61a43901f095b5534d
David Kokkelink <david@datawrapper.de>
2020-06-29 14:15:02 +0200

    checkURL instead of fetchSafely

---
+++ node/checkURL.js
@@ -0,0 +1,9 @@
+async function checkURL(url) {
+    if (url.indexOf('://unix:') > -1 || url.indexOf('unix:') === 0) {
+        return false;
+    }
+
+    return true;
+}
+
+module.exports = checkURL;
